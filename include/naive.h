e192777b829768bf93694befaa4c4346692b1ac9
Devin Qu <devinqu10@gmail.com>
2023-11-30 22:05:18 -0500

    header files and basic tests

---
+++ include/naive.h
@@ -0,0 +1,5 @@
+#include <vector>
+
+std::vector<int> solve_naive(const std::vector<int>& w, const int T, const int l, const int r, bool& is_possible);
+
+bool solve_naive(const std::vector<int>& w, const int T);
---
+++ include/parallel_cpu.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+constexpr int RECURSION_LIMIT = 500;
+constexpr int USE_EXPONENTIIAL = 16;
+
+bool solve_parallel(const std::vector<int>& w, const int T);
