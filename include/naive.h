687a555ef910fd1df89b8e14f34715f94e93bc28
Devin Qu <devinqu10@gmail.com>
2023-12-03 22:57:19 -0500

    create power-of-2 length arrays

--- include/naive.h
+++ include/naive.h
@@ -1,6 +1,6 @@
 #include <cstdint>
 #include <vector>
 
-std::vector<uint32_t> solve_naive(const std::vector<uint32_t>& w, const uint32_t T, const uint32_t l, const uint32_t r, bool& is_possible);
+std::vector<uint32_t> solve_naive(const std::vector<uint32_t>& w, const uint32_t T, const uint32_t T_ceil, const uint32_t l, const uint32_t r, bool& is_possible);
 
 bool solve_naive(const std::vector<uint32_t>& w, const uint32_t T);
--- src/parallel_cpu.cpp
+++ src/parallel_cpu.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <bit>
 
 #include "get_subset_sums.h"
 #include "parallel_cpu.h"
@@ -26,11 +27,11 @@ std::vector<uint32_t> solve_exponential(const std::vector<uint32_t>& w, const ui
     return (is_possible ? std::vector<uint32_t>() : possible);
 }
 
-std::vector<uint32_t> solve_sequential(const std::vector<uint32_t>& w, const uint32_t T, const uint32_t l, const uint32_t r, bool& is_possible) {
+std::vector<uint32_t> solve_sequential(const std::vector<uint32_t>& w, const uint32_t T, const uint32_t T_ceil, const uint32_t l, const uint32_t r, bool& is_possible) {
     const uint32_t len = r - l;
 
     if (len <= USE_NAIVE) {
-        return solve_naive(w, T, l, r, is_possible);
+        return solve_naive(w, T, T_ceil, l, r, is_possible);
     }
 
     /*
@@ -44,47 +45,39 @@ std::vector<uint32_t> solve_sequential(const std::vector<uint32_t>& w, const uin
 
     std::vector<uint32_t> left, right;
 
-    left = solve_sequential(w, T, l, m, is_possible);
+    left = solve_sequential(w, T, T_ceil, l, m, is_possible);
 
     if (is_possible) {
         return {};
     }
 
-    right = solve_sequential(w, T, m, r, is_possible);
+    right = solve_sequential(w, T, T_ceil, m, r, is_possible);
 
     if (is_possible) {
         return {};
     }
 
-    auto convolution = atcoder::convolution(left, right);
+    auto convolution = conv(left, right);
 
-    if (static_cast<uint32_t>(std::size(convolution)) > T + 1) {
-        convolution.resize(T + 1);
+    if (convolution[T]) {
+        is_possible = true;
 
-        if (convolution[T]) {
-            is_possible = true;
-
-            return {};
-        }
-    }
-
-    for (uint32_t& x : convolution) {
-        x = std::min(1U, x);
+        return {};
     }
 
     return convolution;
 }
 
 // maybe switch to naive if sum is small enough
-std::vector<uint32_t> solve(const std::vector<uint32_t>& w, const uint32_t T, const uint32_t l, const uint32_t r, bool& is_possible) {
+std::vector<uint32_t> solve(const std::vector<uint32_t>& w, const uint32_t T, const uint32_t T_ceil, const uint32_t l, const uint32_t r, bool& is_possible) {
     const uint32_t len = r - l;
 
     if (len <= RECURSION_LIMIT) {
-        return solve_sequential(w, T, l, r, is_possible);
+        return solve_sequential(w, T, T_ceil, l, r, is_possible);
     }
 
     if (len <= USE_NAIVE) {
-        return solve_naive(w, T, l, r, is_possible);
+        return solve_naive(w, T, T_ceil, l, r, is_possible);
     }
 
     /*
@@ -99,14 +92,14 @@ std::vector<uint32_t> solve(const std::vector<uint32_t>& w, const uint32_t T, co
     std::vector<uint32_t> left, right;
 
 #pragma omp task shared(w, T, l, r, is_possible, left)
-    left = solve(w, T, l, m, is_possible);
+    left = solve(w, T, T_ceil, l, m, is_possible);
 
     if (is_possible) {
         return {};
     }
 
 #pragma omp task shared(w, T, l, r, is_possible, right)
-    right = solve(w, T, m, r, is_possible);
+    right = solve(w, T, T_ceil, m, r, is_possible);
 
 #pragma omp taskwait
 
@@ -114,20 +107,12 @@ std::vector<uint32_t> solve(const std::vector<uint32_t>& w, const uint32_t T, co
         return {};
     }
 
-    auto convolution = atcoder::convolution(left, right);
+    auto convolution = conv(left, right);
 
-    if (static_cast<uint32_t>(std::size(convolution)) > T + 1) {
-        convolution.resize(T + 1);
+    if (convolution[T]) {
+        is_possible = true;
 
-        if (convolution[T]) {
-            is_possible = true;
-
-            return {};
-        }
-    }
-
-    for (uint32_t& x : convolution) {
-        x = std::min(1U, x);
+        return {};
     }
 
     return convolution;
@@ -135,8 +120,11 @@ std::vector<uint32_t> solve(const std::vector<uint32_t>& w, const uint32_t T, co
 
 bool solve_parallel(const std::vector<uint32_t>& w, const uint32_t T) {
     bool is_possible = false;
+    const uint32_t T_ceil = std::bit_ceil(T + 1);
 
-    solve(w, T, 0, std::size(w), is_possible);
+    conv_init(T_ceil);
+
+    solve(w, T, T_ceil, 0, std::size(w), is_possible);
 
     return is_possible;
 }
